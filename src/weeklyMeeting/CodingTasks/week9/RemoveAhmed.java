72c1c1fb1eb1150ccd381bf29bd73a8f12b12816
merkurt2001 <merkurt2001@gmail.com>
2023-03-29 16:09:38 +0200

    revert demo4

--- src/weeklyMeeting/CodingTasks/week9/RemoveAhmed.java
+++ src/weeklyMeeting/CodingTasks/week9/RemoveAhmed.java
@@ -31,6 +31,11 @@ Write a java operation to remove all the names named Ahmed
         while(it.hasNext()) {
             if(it.next().equals("Ahmed"))
                 it.remove();
+
+
+
+
+
         }
         System.out.println( names5 );
     }
